4465ed6426885ea5844d11bf30c4b103b8fdc21a
rubenwardy <rubenwardy@gmail.com>
2017-03-25 14:41:24 +0000

    Add %d / %i to printf

--- user/stdio.c
+++ user/stdio.c
@@ -6,7 +6,6 @@
 void printf(const char *fmt, ...) {
 	va_list args;
 	va_start(args, fmt);
-	va_end(args);
 
 	char buffer[1024];
 	size_t output_length = 0;
@@ -19,6 +18,7 @@ void printf(const char *fmt, ...) {
 			if (i >= len) {
 				char m[] = "printf: unexpected eos after var";
 				write(STDERR_FILENO, m, strlen(m));
+				va_end(args);
 				return;
 			}
 			c = fmt[i];
@@ -26,18 +26,34 @@ void printf(const char *fmt, ...) {
 			if (c == 's') {
 				char *str = va_arg(args, char*);
 				size_t ptr = 0;
-				while (str[ptr] != '\0') {
+				while (str[ptr] != '\0' && output_length < 1024) {
+					buffer[output_length++] = str[ptr++];
+				}
+			} else if (c == 'd' || c == 'i') {
+				int x = va_arg(args, int);
+				char str[100];
+				itoa(&str[0], x);
+				size_t ptr = 0;
+				while (str[ptr] != '\0' && output_length < 1024) {
 					buffer[output_length++] = str[ptr++];
 				}
 			} else {
 				char m[] = "printf: unrecognised print type var";
 				write(STDERR_FILENO, m, strlen(m));
+				va_end(args);
 				return;
 			}
-		} else {
+		} else if (output_length < 1024) {
 			buffer[output_length++] = c;
 		}
 	}
 
+	va_end(args);
+
 	write(STDOUT_FILENO, buffer, output_length);
+
+	if (output_length > 1024) {
+		char m[] = "printf: buffer overflow";
+		write(STDOUT_FILENO, m, strlen(m));
+	}
 }
