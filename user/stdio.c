39614b5e5a848989a4ee32655bb2a1787443be09
rubenwardy <rubenwardy@gmail.com>
2017-03-25 00:48:00 +0000

    Add stdio.h/c files with simple printf implementation

---
+++ user/stdio.c
@@ -0,0 +1,43 @@
+#include "stdio.h"
+#include <stdarg.h>
+#include <string.h>
+#include "libc.h"
+
+void printf(const char *fmt, ...) {
+	va_list args;
+	va_start(args, fmt);
+	va_end(args);
+
+	char buffer[1024];
+	size_t output_length = 0;
+
+	size_t len = strlen(fmt);
+	for (size_t i = 0; i < len; i++) {
+		char c = fmt[i];
+		if (c == '%') {
+			i++;
+			if (i >= len) {
+				char m[] = "printf: unexpected eos after var";
+				write(STDERR_FILENO, m, strlen(m));
+				return;
+			}
+			c = fmt[i];
+
+			if (c == 's') {
+				char *str = va_arg(args, char*);
+				size_t ptr = 0;
+				while (str[ptr] != '\0') {
+					buffer[output_length++] = str[ptr++];
+				}
+			} else {
+				char m[] = "printf: unrecognised print type var";
+				write(STDERR_FILENO, m, strlen(m));
+				return;
+			}
+		} else {
+			buffer[output_length++] = c;
+		}
+	}
+
+	write(STDOUT_FILENO, buffer, output_length);
+}
---
+++ user/stdio.h
@@ -0,0 +1,6 @@
+#ifndef STDIO_H
+#define STDIO_H
+
+extern void printf(const char *fmt, ...);
+
+#endif
